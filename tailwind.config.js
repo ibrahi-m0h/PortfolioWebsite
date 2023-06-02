03d61456652e8886d7562991e32ab7ef745b3353
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2023-06-02 15:34:35 -0700

    Add files for Github Pages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   'merriweather': ['Merriweather', 'serif']
+      // }
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
